9499d674480d95a2070e745490a38812e3856c68
Jucemar Dimon <jucemar.dimon@gmail.com>
2021-05-23 17:28:29 -0300

    Conclui o capitulo II

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,54 @@
+import { Container } from "./styles";
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { useTransactions } from "../../hooks/useTransactions";
+
+
+
+export function Summary() {
+  const { transactions } = useTransactions();
+  const sumary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total+=transaction.amount;
+    } else {
+      acc.withdraw += transaction.amount;
+      acc.total-=transaction.amount;
+    }
+    return acc;
+  }, { deposits: 0, withdraw: 0, total: 0 });
+
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>
+            Entradas
+                    </p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(sumary.deposits)}</strong>
+      </div>
+      <div>
+        <header>
+          <p>
+            Saídas
+                    </p>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+        <strong>- {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(sumary.withdraw)}</strong>
+      </div>
+      <div className="hightlight-background">
+        <header>
+          <p>
+            Total
+                    </p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(sumary.total)}</strong>
+      </div>
+
+    </Container>
+  )
+}
